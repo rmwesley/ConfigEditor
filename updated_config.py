7676f80326b98411c66c7375a3644886a7a8057a
Wesley <wesley.machado.2018@polytechnique.org>
2023-06-10 05:46:33 +0200

    Updater now handles array indexing as well

--- updated_config.py
+++ updated_config.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import re
 
 config_filename = sys.argv[1]
 updates_filename = sys.argv[2]
@@ -8,16 +9,44 @@ def update(path, dict_obj, new_value):
     keys = path.split('.')
     last_key = keys.pop()
     current = dict_obj
+
     for key in keys:
-        current = current[key]
-    current[last_key] = new_value
+        # Check if "key" has array indexing at its end
+        if(key[-1] == ']'):
+            # Get literal part to the left of first opening square bracket
+            literal_part = key.split('[', 1)[0]
+            current = current[literal_part]
+
+            # Regular expression to find all array indices
+            # That is, integers enclosed in square brackets, like [2]
+            indices = re.findall(r"\[(\d+)\]", key)
+            for idx in indices:
+                current = current[int(idx)]
+        else:
+            current = current[key]
+    # Check if "last_key" has array indexing at its end
+    if(last_key[-1] == ']'):
+        # Get literal part to the left of first opening square bracket
+        literal_part = last_key.split('[', 1)[0]
+        current = current[literal_part]
+
+        # Regular expression to find all array indices
+        # That is, integers enclosed in square brackets, like [2]
+        indices = re.findall(r'\[(\d+)\]', last_key)
+        last_index = indices.pop()
+
+        for idx in indices:
+            current = current[int(idx)]
+        current[int(last_index)] = new_value
+    else:
+        current[last_key] = new_value
 
 with open(config_filename) as conf_file:
     json_conf = json.load(conf_file)
 
 with open(updates_filename) as changes_file:
     for line in changes_file:
-        left, right = line.split('": "', 1)
+        left, right = line.split('": ', 1)
         path = left[1:]
         change_value = right[:-1]
 
