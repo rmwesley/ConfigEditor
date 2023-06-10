958abd40bec7aa8edbe29bdce4db0d5a7bfaa2b4
Wesley <wesley.machado.2018@polytechnique.org>
2023-06-10 04:57:48 +0200

    Config updater now takes in filenames as arguments

--- updated_config.py
+++ updated_config.py
@@ -1,4 +1,8 @@
+import sys
 import json
+
+config_filename = sys.argv[1]
+updates_filename = sys.argv[2]
 
 def update(path, dict_obj, new_value):
     keys = path.split('.')
@@ -8,13 +12,14 @@ def update(path, dict_obj, new_value):
         current = current[key]
     current[last_key] = new_value
 
-with open("config.json") as conf_file:
+with open(config_filename) as conf_file:
     json_conf = json.load(conf_file)
 
-with open("changes.txt") as changes_file:
+with open(updates_filename) as changes_file:
     for line in changes_file:
         left, right = line.split('": "', 1)
         path = left[1:]
         change_value = right[:-1]
+
         update(path, json_conf, change_value)
 print(json_conf)
